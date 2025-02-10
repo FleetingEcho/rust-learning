4778f9610cfb7e8677ab4c5b0a5e05195bea4ab4
JakeZT <jakeottawaca@gmail.com>
2025-02-09 22:38:36 -0800

    chore: add  all patterns

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -4,20 +4,22 @@ mod config;
 mod base_type;
 mod my_struct;
 mod my_match;
+mod all_pattern;
 // use utils::helper::print_max_points;
 
 fn main() {
-    variable::const_test("Rust");
+    // variable::const_test("Rust");
 
-    let upper = utils::to_uppercase("rust");
-    println!("Uppercase: {}", upper);
+    // let upper = utils::to_uppercase("rust");
+    // println!("Uppercase: {}", upper);
 
-    let sum = utils::add(10, 20);
-    println!("Sum: {}", sum);
+    // let sum = utils::add(10, 20);
+    // println!("Sum: {}", sum);
 
-    utils::helper::print_max_points();
+    // utils::helper::print_max_points();
 
-    base_type::base_type_main();
-    my_struct::test();
-    my_match::match_if();
+    // base_type::base_type_main();
+    // my_struct::test();
+    // my_match::match_if();
+    all_pattern::test();
 }
