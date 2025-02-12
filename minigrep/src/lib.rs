ca62ee2a14dfa0762e0a847df6802e81e23fa4d8
JakeZT <jakeottawaca@gmail.com>
2025-02-11 21:23:03 -0800

    chore: update minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -73,30 +73,21 @@ pub fn search<'a>(query: &str, contents: &'a str, ignore_case: bool) -> Vec<&'a 
 
 
 #[cfg(test)]
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn case_sensitive() {
         let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents,false));
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents, false));
     }
 
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents,true));
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents, true));
     }
 }
